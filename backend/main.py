cebcaed0061e733b06eb0c42e0d5b654bea66b47
Jenushan <jenushan349@gmail.com>
2026-09-07 15:22:51 -0400

    fix: allow vercel frontend origin

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:3000", "http://127.0.0.1:3000",], allow_credentials=True, allow_methods=["*"], allow_headers=["*"],)
+app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:3000", "http://127.0.0.1:3000","https://bookmark-manager-five-flax.vercel.app",], allow_credentials=True, allow_methods=["*"], allow_headers=["*"],)
 
 @app.get("/")
 def home(): 
